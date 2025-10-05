710e6dbc52133ac39fade69b5558a47b3cfb28bb
CzuczorLajos-Instructor <czuczor.lajos@hegersor.hu>
2025-10-05 21:19:03 +0200

    Add CSV data processing and Table struct implementation
    Updated `Main` in `Program.cs` to read table data from a CSV file and handle invalid lines. Added `ReadTableData` and `Query` methods, with `Query` currently unimplemented. Introduced a `Table` struct in `Table.cs` with properties (`Name`, `Length`, `Width`, `Price`), a constructor, and an `Area` method to calculate the table's area.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,37 @@ internal class Program
         string projectFolder = Path.Combine(AppContext.BaseDirectory, "../../..");
         string dataFile = Path.GetFullPath(Path.Combine(projectFolder, relDataFile)); // combine then normalise
 
-        // set up data structure and populate with table data by reading CSV file
-        // perform queries and output results
+        List<Table> tables = ReadTableData(dataFile);
+        Query(tables);
     }
+
+    private static List<Table> ReadTableData(string dataFile)
+    {
+        List<Table> data = ReadTableData0(dataFile, out List<string> invalidLines);
+        if (invalidLines.Count == 0) { Console.WriteLine($"1. : A fájl hibátlan, nincs problémás adatsora."); }
+        else
+        {
+            Console.WriteLine($"1. : A fájlban {invalidLines.Count} adatsor nem értelmezhető, ez(ek) a következő(k):");
+            foreach (string line in invalidLines) { Console.WriteLine($"    {line}"); }
+        }
+        return data;
+    }
+
+    private static void Query(List<Table> tables)
+    {
+        throw new NotImplementedException();
+    }
+
+    //**********************************************************************************
+    // Read table data from CSV file
+    //
+    private static List<Table> ReadTableData0(string dataFile, out List<string> invalidLines)
+    {
+        throw new NotImplementedException();
+    }
+
+    //**********************************************************************************
+    // Query table data
+    //
+
 }
---
+++ Table.cs
@@ -0,0 +1,21 @@
+﻿namespace CsvFileProcessing;
+internal struct Table
+{
+    internal string Name { get; }
+    internal int Length { get; }
+    internal int Width { get; }
+    internal int Price { get; }
+
+    internal Table(string name, int length, int width, int price)
+    {
+        Name = name;
+        Length = length;
+        Width = width;
+        Price = price;
+    }
+
+    internal int Area()
+    {
+        return Length * Width;
+    }
+}
