1050215a54d1c0b14090a5e4a2c14c976c136985
CzuczorLajos-Instructor <czuczor.lajos@hengersor.hu>
2025-10-06 18:58:50 +0200

    Add second constructor to Table and validation too

--- Table.cs
+++ Table.cs
@@ -1,4 +1,5 @@
-﻿namespace CsvFileProcessing;
+﻿
+namespace CsvFileProcessing;
 internal struct Table
 {
     internal string Name { get; }
@@ -8,18 +9,33 @@ internal struct Table
 
     internal Table(string name, int length, int width, int price)
     {
-        ArgumentException.ThrowIfNullOrWhiteSpace(name);
-        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(length);
-        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(width);
-        ArgumentOutOfRangeException.ThrowIfNegative(price);
         Name = name;
         Length = length;
         Width = width;
         Price = price;
+        Validate();
+    }
+
+    internal Table(string csvLine)
+    {
+        string[] values = csvLine.Split(';');
+        Name = values[0].Trim();
+        Length = int.Parse(values[1].Trim());
+        Width = int.Parse(values[2].Trim());
+        Price = int.Parse(values[3].Trim());
+        Validate();
     }
 
     internal int Area()
     {
         return Length * Width;
+    }
+
+    private void Validate()
+    {
+        if (Name == null || Name == "") throw new ArgumentException("A név nem lehet üres");
+        if (Length <= 0) throw new ArgumentOutOfRangeException("A hossz nem lehet 0 vagy negatív");
+        if (Width <= 0) throw new ArgumentOutOfRangeException("A szélesség nem lehet 0 vagy negatív");
+        if (Price < 0) throw new ArgumentOutOfRangeException("Az ár nem lehet negatív");
     }
 }
